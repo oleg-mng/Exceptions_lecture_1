59965073f1f2a9d6876682347ae38bab739873a1
oleg <legik7127@gmail.com>
2023-03-27 19:28:48 +0600

    added method divide

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,26 +1,38 @@
+import java.io.File;
+
 public class Program {
 
         public static void main(String[] args) {
-            divide(10, 0);
+//            a();
+//            c();
+            System.out.println(divide(10,0));
+//            System.out.println(getFileSize(new File("123")));
+//            System.out.println(getFileSize(new File("/Users/olegmonogarov/GeekBrains_/01.07 Колесо компетенций.pdf")));
+        }
+
+        public static long getFileSize(File file){
+            if (!file.exists()){
+                return -1L;
+            }
+            return file.length();
         }
         public static int divide(int a1, int a2) {
             if (a2 == 0) {
-                throw new RuntimeException("Arrays not equals");
+                throw new RuntimeException("Divide by zero not permited");
             }
             return a1 / a2;
         }
-//        a();
-// }
-//    public static void a(){
-//        b();
-//    }
-//    public static void b(){
-//        c();
-//    }
-//    public static void c(){
-//        int [] anw = new int[10];
-//        System.out.println(anw[1000]);
-//    }
+
+    public static void a(){
+        b();
+    }
+    public static void b(){
+        c();
+    }
+    public static void c(){
+        int [] anw = new int[10];
+        System.out.println(anw[1000]);
+    }
 //        System.out.println(getFileSize(new File("/Users/olegmonogarov/GeekBrains_/01.07 Колесо компетенций.pdf")));
 //        System.out.println(divide(10,0));
 //    }
@@ -30,12 +42,7 @@ public class Program {
 //        }
 //        return a1/a2;
 //    }
-//    public static long getFileSize(File file){
-//        if (!file.exists()){
-//            return -1;
-//        }
-//        return file.length();
-//    }
+
 }
 
 
